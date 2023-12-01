ab1c72b6558b83bc14f1f6025c8d4bb076d4a8bd
noahwekesa <noahwekesa0@gmail.com>
2023-12-02 01:59:08 +0300

    how tp use middleware

--- src/parceldelivery/settings.py
+++ src/parceldelivery/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middleware.ProfileMiddleware',
 ]
 
 ROOT_URLCONF = 'parceldelivery.urls'
